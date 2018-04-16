ca7472f3492987259c1fd281f17145708fc3ea1e
Evan <evanmurray2012@gmail.com>
2018-04-16 15:05:07 +0100

    Update auth.js

--- Public/Javascripts/auth.js
+++ Public/Javascripts/auth.js
@@ -1,1 +1,66 @@
+$(document).ready(
+    function() {        
+        /**
+         * Event handler for when the user attempts to register
+         */
+        $("#reg-form").submit(function (event) {
+            event.preventDefault();
+            $.ajax({
+                type: 'POST',
+                url: '/users/register',
+                dataType: 'json',
+                data: {
+                    'user_name': event.target.inputUsername.value,
+                    'password': event.target.inputPassword.value
+                },
+                success: function(token){
+		 $(location).attr('href', '/feed' );	// Redirect to a login page
+                },
+                error: function(errMsg) {
+                    swal(
+                        'Oops...',
+                        errMsg.responseJSON.body,
+                        'error'
+                    )
+                }
+              });
+        }); });
+
+$(document).ready(
+    function() {        
+        /**
+         * Event handler for when the user attempts to register
+         */
+        $("#log-form").submit(function (event) {
+            event.preventDefault();
+            $.ajax({
+                type: 'POST',
+                url: '/users/login',
+                dataType: 'json',
+                data: {
+                    'user_name': event.target.inputUsername.value,
+                    'password': event.target.inputPassword.value
+                },
+                success: function(token){
+		 $(location).attr('href', '/feed' );	// Redirect to a login page
+                },
+                error: function(errMsg) {
+                    swal(
+                        'Oops...',
+                        errMsg.responseJSON.body,
+                        'error'
+                    )
+                }
+              });
+        }); });
+
+
+
+$("#logout").click(function (event) {
+
+	Cookies.remove('Authorization');            
+	$(location).attr('href', '/');
+});
+
+
 
