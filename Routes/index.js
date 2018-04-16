505f652d8d8324ec4abb2b16890b1aa250b94cbd
Evan <evanmurray2012@gmail.com>
2018-04-16 15:06:58 +0100

    Update index.js

--- Routes/index.js
+++ Routes/index.js
@@ -1,1 +1,64 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/users');
+var jwt = require('jsonwebtoken');
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/register', function(req, res, next) {
+    res.render('register');
+});
+
+router.get('/login', function(req, res, next) {
+    res.render('login');
+});
+
+router.post('/register', function(req, res, next){
+    var username = req.body.user_name;
+    var password = req.body.password;
+    // Check if account already exists
+    User.findOne({ 'user_name' :  username }, function(err, user)
+    {
+        if (err)
+            res.send(err);
+        // check to see if theres already a user with that email
+        if (user) {
+            res.status(401).json({
+                "status": "info",
+                "body": "Username already taken"
+            });
+        } else {
+            // If there is no user with that username create the user
+            var newUser = new User();
+
+            // set the user's local credentials
+            newUser.user_name = username;
+            newUser.password = newUser.generateHash(password);
+            newUser.access_token = createJwt({user_name:username});
+            newUser.save(function(err, user) {
+                if (err)
+                    throw err;
+	     res.cookie('Authorization', 'Bearer ' + user.access_token); 
+                res.json({'success' : 'account created'});
+
+            });
+        }
+    });
+});
+
+/*
+ Creates a JWT
+ */
+function createJwt(profile) {
+    return jwt.sign(profile, 'CSIsTheWorst', {
+        expiresIn: '10d'
+    });
+}
+
+
+module.exports = router;
 
